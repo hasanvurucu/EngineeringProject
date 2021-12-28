12c799e6d7c3c6dfe925b1366e5678110a2cd391
Hasan Vurucu <tr.yusufpolat@gmail.com>
2021-12-28 12:52:06 +0300

    Breaking system prototype

---
+++ Assets/Scripts/PlayerAbilities.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAbilities : MonoBehaviour
+{
+    private GameObject InRangeObject;
+
+    [SerializeField] private Button InteractButton;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Tree")
+        {
+            InRangeObject = other.transform.parent.gameObject;
+
+            InteractButton.interactable = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Tree")
+        {
+            InRangeObject = null;
+
+            InteractButton.interactable = false;
+        }
+    }
+
+    public void BreakOtherObject()
+    {
+        if(InRangeObject != null)
+        {
+            GameObject temp = InRangeObject;
+            StartCoroutine(BreakingProcess(temp));
+            InRangeObject.transform.GetChild(1).gameObject.GetComponent<Collider>().enabled = false;
+
+            InteractButton.interactable = false;
+        }
+    }
+
+
+
+    IEnumerator BreakingProcess(GameObject chosenToBreak)
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        GameObject[] Groups = {
+            chosenToBreak.transform.GetChild(0).GetChild(0).gameObject,
+            chosenToBreak.transform.GetChild(0).GetChild(1).gameObject,
+            chosenToBreak.transform.GetChild(0).GetChild(2).gameObject };
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < Groups[i].transform.childCount; j++)
+            {
+                Groups[i].transform.GetChild(j).gameObject.AddComponent<MeshCollider>().convex = true;
+                Groups[i].transform.GetChild(j).gameObject.AddComponent<Rigidbody>();
+                StartCoroutine(DestroyAfterSeconds(Groups[i].transform.GetChild(j).gameObject));
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+
+    }
+
+    IEnumerator DestroyAfterSeconds(GameObject given)
+    {
+        yield return new WaitForSeconds(1.3f);
+        Destroy(given);
+
+    }
+}
--- Assets/Scripts/UIfunctions.cs
+++ Assets/Scripts/UIfunctions.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class UIfunctions : MonoBehaviour
 {
+    [SerializeField] private PlayerAbilities playerAbilities;
+
     public void Interact()
     {
         Debug.Log("Interacted");
+
+        playerAbilities.BreakOtherObject();
     }
 }
