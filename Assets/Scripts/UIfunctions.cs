18e74a9ca25d4f0a5a5c56de05a1506dba927e6b
Hasan Vurucu <tr.yusufpolat@gmail.com>
2021-12-28 10:31:24 +0300

    Models Import, Basic Interact log

---
+++ Assets/Scripts/UIfunctions.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIfunctions : MonoBehaviour
+{
+    public void Interact()
+    {
+        Debug.Log("Interacted");
+    }
+}
