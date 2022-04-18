5d34931279df99e4bafc4d60fc77dbf92ae5d442
karacamu <karacaamuhammed@gmail.com>
2022-04-19 02:17:37 +0300

    Crafting Table
    -Approaching to the table
    -Panel appearence

--- Assets/Scripts/PlayerCollisions.cs
+++ Assets/Scripts/PlayerCollisions.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCollisions : MonoBehaviour
 {
+    [SerializeField] private Transform craftingPanel;
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Wood")
@@ -14,6 +17,45 @@ public class PlayerCollisions : MonoBehaviour
         if(collision.gameObject.tag == "Stone")
         {
             CollectCollided(Tags.StoneAmountTag, collision.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "CraftingTable")
+        {
+            StopAllCoroutines();
+            StartCoroutine(AppearDisappear(true));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "CraftingTable")
+        {
+            StopAllCoroutines();
+            StartCoroutine(AppearDisappear(false));
+        }
+    }
+
+    IEnumerator AppearDisappear(bool isAppearing)
+    {
+        float t = 0;
+
+        Vector3 targetSize;
+
+        if (isAppearing)
+            targetSize = Vector3.one;
+        else
+            targetSize = Vector3.zero;
+
+        while(t < 1)
+        {
+            t += Time.deltaTime;
+
+            craftingPanel.localScale = Vector3.Lerp(craftingPanel.localScale, targetSize, t);
+
+            yield return new WaitForEndOfFrame();
         }
     }
 
