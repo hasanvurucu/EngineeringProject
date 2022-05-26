de645e9dc46f4e55e40204a434b91c92394c5389
hasanvurucu <47665574+hasanvurucu@users.noreply.github.com>
2022-05-26 20:23:44 +0300

    Additions (See comments)
    Erlik animation added
    Script structure is added for the quest system

---
+++ Assets/Scripts/QuestInfo.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestInfo : MonoBehaviour
+{
+    private string[] QuestDialogs = new string[99];
+
+    private void Awake()
+    {
+        QuestDialogs[0] = " ";
+    }
+
+    /*
+     
+    (Malzeme toplama görevi)
+    Altay:Here ý am Erlik, my people are in famine and they are in very bad shape. I want you to get them out of this situation
+    Erlik:I can’t save them young soldier,but maybe you can.
+    Altay: What do you mean ?
+    Erlik: Although your people have lived in abundance for a long time, it was not enough for them.They wanted more and started to steal from others.So I unleashed this scourge upon them.But ý will give them a second chance. if you can complete the tasks that i will give you, I will remove the famine of your people.
+    Altay:Okey,tell me and I will do it!
+    Erlik:Your first task is to collect some elements for me.I need the sap of the sacred trees that grow in the nearby forest.Go there and get me what I need!
+
+
+    (Düþman kesme görevi)
+    Altay:Here your tree saps,now save my village.
+    Erlik:Not so fast young soldier. Congratulations on completing your first task.But I have another job for you.If you can complete it then i will save your people.
+    Altay:What is it ?
+    Erlik: There's a creature in the north. This creature damages the crops of the people there and kidnaps their animals. Kill this creature, save the people there, and I'll save your people.
+
+    (Yaratýðý öldürdük geldik)
+    Altay:Erlik! I killed the creature.
+    Erlik:Well done young soldier. Nice to see you're a man of your word. Now that you've learned to help others, I agree to help you. return to your village and enjoy the abundance
+
+
+    */
+}
