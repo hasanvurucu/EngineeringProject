b958e30535574f674af3d87c8f366d6b49e284ae
hasanvurucu <47665574+hasanvurucu@users.noreply.github.com>
2022-06-22 02:54:37 +0300

    Quest infos are inserted into string array

--- Assets/Scripts/QuestHandler.cs
+++ Assets/Scripts/QuestHandler.cs
@@ -15,4 +15,13 @@ public class QuestHandler : MonoBehaviour
     {
         
     }
+
+    /*
+    
+    -Get string array from quest info
+    -Create a UI object and receive string array values into text object. Put line breaks if necessary.
+    -Enable "Next" button until the necessary point, then check for quest necessities.
+    -Walk through string array until all quests are done, when the quests are done, hide "Next" button completely.
+
+    */
 }
